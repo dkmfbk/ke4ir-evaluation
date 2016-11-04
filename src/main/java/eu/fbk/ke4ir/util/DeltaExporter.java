e60af1667cd40bffe8417ab784d3337cb6edf6dc
Francesco Corcoglioniti <francesco.corcoglioniti@gmail.com>
2016-11-04 17:21:26 +0100

    Added script for extracting deltas w.r.t. textual

---
+++ src/main/java/eu/fbk/ke4ir/util/DeltaExporter.java
@@ -0,0 +1,138 @@
+package eu.fbk.ke4ir.util;
+
+import java.io.BufferedReader;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import eu.fbk.rdfpro.util.IO;
+
+public class DeltaExporter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DeltaExporter.class);
+
+    private static final Map<String, Integer> MEASURES = ImmutableMap.<String, Integer>builder()
+            .put("p1", 1).put("p3", 2).put("p5", 3).put("p10", 4).put("mrr", 5).put("ndcg", 6)
+            .put("ndcg10", 7).put("map", 8).put("map10", 9).build();
+
+    public static void main(final String... args) {
+        try {
+            // Parse command line
+            final CommandLine cmd = CommandLine.parser().withName("delta-exporter")
+                    .withOption("i", "input", "the input folder", "PATH",
+                            CommandLine.Type.DIRECTORY_EXISTING, true, false, true)
+                    .withOption("o", "output", "the output folder", "PATH",
+                            CommandLine.Type.DIRECTORY_EXISTING, true, false, true)
+                    .withHeader("Computes deltas of layer+textual wrt textual only, "
+                            + "for all queries in one or more folders")
+                    .withLogger(LoggerFactory.getLogger("eu.fbk")).parse(args);
+
+            // Read options
+            final Path inputPath = cmd.getOptionValue("i", Path.class);
+            final Path outputPath = cmd.getOptionValue("o", Path.class);
+
+            // Allocate a map of writers for output files
+            final Map<String, Writer> writers = Maps.newHashMap();
+
+            try {
+                // Open output files
+                for (final String measure : MEASURES.keySet()) {
+                    final Path path = outputPath.resolve("delta." + measure + ".csv");
+                    writers.put(measure, IO.utf8Writer(IO.buffer(IO.write(path.toString()))));
+                }
+
+                // Process all sub-folders of the specified input folder
+                for (final Path folderPath : Files.list(inputPath)
+                        .filter(p -> Files.isDirectory(p)).collect(Collectors.toList())) {
+
+                    // Compute and log folder ID
+                    final String folderId = folderPath.getFileName().toString();
+                    LOGGER.info("Processing folder {}", folderId);
+
+                    // Process all query files in the current folder
+                    for (final Path queryPath : Files.list(folderPath)
+                            .filter(p -> p.getFileName().toString().startsWith("query-"))
+                            .collect(Collectors.toList())) {
+
+                        // Compute and log query ID
+                        final String queryId = queryPath.getFileName().toString()
+                                .substring("query-".length());
+                        LOGGER.info("Processing query {}", queryId);
+
+                        // Extract all rows for the layer settings effectively available (column 11)
+                        final Map<String, String[]> rows = Maps.newHashMap();
+                        try (BufferedReader reader = new BufferedReader(
+                                IO.utf8Reader(IO.buffer(IO.read(queryPath.toString()))))) {
+                            String line;
+                            while ((line = reader.readLine()) != null) {
+                                final String[] fields = line.split(";");
+                                if (fields.length >= 12) {
+                                    final String setting = fields[11].replace(' ', ',');
+                                    if (setting.contains("textual")
+                                            && !rows.containsKey(setting)) {
+                                        rows.put(setting, fields);
+                                    }
+                                }
+                            }
+                        }
+
+                        // Retrieve the row for the textual setting
+                        final String[] textualRow = rows.get("textual");
+                        if (textualRow == null) {
+                            LOGGER.warn("No data for 'textual' setting in file " + queryPath);
+                            continue;
+                        }
+
+                        // Compute deltas and update output delta files
+                        for (final Entry<String, String[]> entry : rows.entrySet()) {
+                            final String setting = entry.getKey();
+                            final String[] row = entry.getValue();
+                            if (!"textual".equals(setting)) {
+                                for (final String measure : MEASURES.keySet()) {
+                                    final int index = MEASURES.get(measure);
+                                    final double settingValue = Double.parseDouble(row[index]);
+                                    final double textualValue = Double
+                                            .parseDouble(textualRow[index]);
+                                    final double delta = delta(settingValue, textualValue);
+                                    final Writer writer = writers.get(measure);
+                                    writer.write(folderId + ";" + queryId + ";" + setting + ";"
+                                            + delta + ";" + settingValue + ";" + textualValue
+                                            + "\n");
+                                }
+                            }
+                        }
+                    }
+                }
+
+            } finally {
+                // Close output files
+                for (final Writer writer : writers.values()) {
+                    writer.close();
+                }
+            }
+
+        } catch (final Throwable ex) {
+            // Display error information and terminate
+            CommandLine.fail(ex);
+        }
+    }
+
+    private static double delta(final double value, final double baseline) {
+        if (value == baseline) {
+            return 0.0;
+        } else {
+            final double mean = 0.5 * (value + baseline);
+            return (value - baseline) / mean;
+        }
+    }
+
+}
